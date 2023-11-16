c77f6a135d302b15e7a4f27a283952a03cecbdf7
Euwamahoro <e.uwamahoro@alustudent.com>
2023-11-16 23:40:59 +0200

    This is Enock

---
+++ ssh/add_public_key.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Define the server details
+SERVER_IP="34.229.92.198"
+SSH_KEY="~/.ssh/school"
+PUBLIC_KEY="AAAAB3NzaC1yc2EAAAADAQABAAABAQDNdtrNGtTXe5Tp1EJQop8mOSAuRGLjJ6DW4PqX4wId/Kawz35ESampIqHSOTJmbQ8UlxdJuk0gAXKk3Ncle4safGYqM/VeDK3LN5iAJxf4kcaxNtS3eVxWBE5iF3FbIjOqwxw5Lf5sRa5yXxA8HfWidhbIG5TqKL922hPgsCGABIrXRlfZYeC0FEuPWdr6smOElSVvIXthRWp9cr685KdCI+COxlj1RdVsvIo+zunmLACF9PYdjB2s96Fn0ocD3c5SGLvDOFCyvDojSAOyE70ebIElnskKsDTGwfT4P6jh9OBzTyQEIS2jOaE5RQq4IB4DsMhvbjDSQrP0MdCLgwkN"
+
+# Connect to the server and add the public key to authorized_keys
+ssh -i $SSH_KEY ubuntu@$SERVER_IP <<EOF
+  # Add public key to authorized_keys
+  echo "$PUBLIC_KEY" >> ~/.ssh/authorized_keys
+
+  # Set appropriate permissions
+  chmod 700 ~/.ssh
+  chmod 600 ~/.ssh/authorized_keys
+EOF
